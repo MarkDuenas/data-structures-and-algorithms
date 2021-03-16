98c2fa76e07ac05f63c5012c4cdf015b04aac5c0
Mark Duenas <markgdlduenas@gmail.com>
2021-03-16 14:50:22 -0700

    completed CC 08

--- javascript/linkedList/index.js
+++ javascript/linkedList/index.js
@@ -1,22 +1,31 @@
 'use strict';
 
 const SLList = require('./lib/linked-list.js');
+const zipLists = require('./llZip/ll-zip.js');
 
-let list = new SLList();
-console.log(list);
+// let list = new SLList();
+// console.log(list);
 
-list.insert(5).insert(1).insert(8);
+// list.insert(5).insert(1).insert(8);
 
-list.append(99);
-list.insertBefore(99, 123);
-list.insertAfter(99, 1000);
-list.insertAfter(5, 596);
-list.insertBefore(5, 8008);
-list.insertBefore(8, 789);
+// list.append(99);
+// list.insertBefore(99, 123);
+// list.insertAfter(99, 1000);
+// list.insertAfter(5, 596);
+// list.insertBefore(5, 8008);
+// list.insertBefore(8, 789);
 
-list.delete(8008);
-list.append(8734);
-console.log(list.length());
-list.toString();
-console.log(list.kthFromEnd(4));
-console.log(list.includes(8));
+// list.delete(8008);
+// list.append(8734);
+// console.log(list.length());
+// list.toString();
+// console.log(list.kthFromEnd(4));
+// console.log(list.includes(8));
+
+const first = new SLList().append('AAA').append('BBB').append('CCC').append('DDD').append('EEE');
+const second = new SLList().append('111').append('222').append('333').append('444').append('555').append('666').append('777');
+
+let zip = zipLists(first, second);
+zip.toString();
+
+console.log(zip.length());
--- javascript/linkedList/llZip/ll-zip.js
+++ javascript/linkedList/llZip/ll-zip.js
@@ -1,53 +1,90 @@
 'use strict';
 
-const SLList = require('../lib/linked-list.js');
-
 function zipLists(list1, list2) {
-  let len1 = list1.length();
-  let len2 = list2.length();
-  let newList = new SLList();
+  // let len1 = list1.length();
+  // let len2 = list2.length();
+  // let newList = new SLList();
 
-  let count;
+  // let count;
 
-  if(len1 < len2) {
-    count = len1;
-  } else {
-    count = len2;
-  }
+  // if(len1 < len2) {
+  //   count = len1;
+  // } else {
+  //   count = len2;
+  // }
 
-  let superList = list1.head;
-  let current2 = list2.head;
+  // let superList = list1.head;
+  // let current2 = list2.head;
 
 
-  while(count > 0) {
+  // while(count > 0) {
 
-    count --;
+  //   count --;
 
-    newList.append(superList.value);
-    superList = superList.next;
+  //   newList.append(superList.value);
+  //   superList = superList.next;
 
-    newList.append(current2.value);
-    current2 = current2.next;
+  //   newList.append(current2.value);
+  //   current2 = current2.next;
 
-  }
+  // }
 
-  if(superList) {
+  // if(superList) {
 
-    while(superList) {
-      newList.append(superList.value);
-      superList = superList.next;
+  //   while(superList) {
+  //     newList.append(superList.value);
+  //     superList = superList.next;
+  //   }
+
+  // } else {
+
+  //   while (current2) {
+  //     newList.append(current2.value);
+  //     current2 = current2.next;
+  //   }
+
+  // }
+
+  // return newList;
+
+  if(!list2.head || !list1.head ){
+    return false;
+  } 
+
+  let firstCurrent = list1.head;
+  let secondCurrent = list2.head;
+  let firstNext;
+  let secondNext;
+
+  while(firstCurrent && secondCurrent) {
+
+    firstNext = firstCurrent.next;
+    secondNext = secondCurrent.next;
+
+    // secondCurrent.next = firstNext;
+    // firstCurrent.next = secondCurrent;
+
+    if (firstNext){
+      secondCurrent.next = firstNext;
+      firstCurrent.next = secondCurrent;
     }
 
-  } else {
-
-    while (current2) {
-      newList.append(current2.value);
-      current2 = current2.next;
+    else if(firstNext === null) {
+      break;
     }
 
+
+    firstCurrent = firstNext;
+    secondCurrent = secondNext;
   }
 
-  return newList;
+  if(secondCurrent) {
+    firstCurrent.next = secondCurrent;
+  }
+
+  list2.head = secondCurrent;
+
+  return list1;
 
 }
 
